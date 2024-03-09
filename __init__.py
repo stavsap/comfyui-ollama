60596da8ce0162c0de187c7fbf542417284f1031
Stav Sapir <stavsap@gmail.com>
2024-03-09 23:39:28 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,30 @@
 from .CompfyuiOllama import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import sys
+import os
+import shutil
+import subprocess
+import threading
+import locale
+import traceback
+
+def process_wrap(cmd_str, cwd=None, handler=None):
+    print(f"[ComfyUI Ollama] EXECUTE: {cmd_str} in '{cwd}'")
+    process = subprocess.Popen(cmd_str, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, bufsize=1)
+
+    if handler is None:
+        handler = handle_stream
+
+    stdout_thread = threading.Thread(target=handler, args=(process.stdout, True))
+    stderr_thread = threading.Thread(target=handler, args=(process.stderr, False))
+
+    stdout_thread.start()
+    stderr_thread.start()
+
+    stdout_thread.join()
+    stderr_thread.join()
+
+    return process.wait()
+
 
 pip_install = []
 
@@ -11,5 +36,6 @@ else:
     mim_install = [sys.executable, '-m', 'mim', 'install']
 
 print(pip_install)
+print(os.path.dirname(__file__))
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
