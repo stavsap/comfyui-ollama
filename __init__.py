d4a07f3d0cfd6bb5156720e74f33a6c55a3497ab
Stav Sapir <stavsap@gmail.com>
2024-03-09 22:48:09 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,17 +1,3 @@
-import importlib.util
-import sys
-import os
-import pathlib
+from .CompfyuiOllama import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
-
-name = "test.py"
-spec = importlib.util.spec_from_file_location(name, os.path.join(pathlib.Path(__file__).parent.resolve(),name))
-module = importlib.util.module_from_spec(spec)
-sys.modules[name] = module
-spec.loader.exec_module(module)
-NODE_CLASS_MAPPINGS.update(module.NODE_CLASS_MAPPINGS)
-NODE_DISPLAY_NAME_MAPPINGS.update(module.NODE_DISPLAY_NAME_MAPPINGS)
-
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
