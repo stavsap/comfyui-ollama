a55dcecde88ad8c187bcd86c48d7f8785a085817
Stav Sapir <stavsap@gmail.com>
2024-03-09 23:29:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,14 @@
 from .CompfyuiOllama import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
+pip_install = []
+
+if "python_embeded" in sys.executable or "python_embedded" in sys.executable:
+    pip_install = [sys.executable, '-s', '-m', 'pip', 'install']
+    mim_install = [sys.executable, '-s', '-m', 'mim', 'install']
+else:
+    pip_install = [sys.executable, '-m', 'pip', 'install']
+    mim_install = [sys.executable, '-m', 'mim', 'install']
+
+print(pip_install)
+
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
