3a5b75162e9150e75b5b71424a2d8fae0185d607
Stav Sapir <stavsap@gmail.com>
2024-03-10 13:00:49 +0200

    fixes
    Signed-off-by: Stav Sapir <stavsap@gmail.com>

--- CompfyuiOllama.py
+++ CompfyuiOllama.py
@@ -6,65 +6,25 @@ import base64
 from io import BytesIO
 
 class OllamaVision:
-    """
-    A Ollama Vision node
-
-    Class methods
-    -------------
-    INPUT_TYPES (dict): 
-        Tell the main program input parameters of nodes.
-
-    Attributes
-    ----------
-    RETURN_TYPES (`tuple`): 
-        The type of each element in the output tulple.
-    RETURN_NAMES (`tuple`):
-        Optional: The name of each output in the output tulple.
-    FUNCTION (`str`):
-        The name of the entry-point method. For example, if `FUNCTION = "execute"` then it will run Example().execute()
-    OUTPUT_NODE ([`bool`]):
-        If this node is an output node that outputs a result/image from the graph. The SaveImage node is an example.
-        The backend iterates on these output nodes and tries to execute all their parents if their parent graph is properly connected.
-        Assumed to be False if not present.
-    CATEGORY (`str`):
-        The category the node should appear in the UI.
-    execute(s) -> tuple || None:
-        The entry point method. The name of this method must be the same as the value of property `FUNCTION`.
-        For example, if `FUNCTION = "execute"` then this method's name must be `execute`, if `FUNCTION = "foo"` then it must be `foo`.
-    """
     def __init__(self):
         pass
     
     @classmethod
     def INPUT_TYPES(s):
-        """
-            Return a dictionary which contains config for all input fields.
-            Some types (string): "MODEL", "VAE", "CLIP", "CONDITIONING", "LATENT", "IMAGE", "INT", "STRING", "FLOAT".
-            Input types "INT", "STRING" or "FLOAT" are special values for fields on the node.
-            The type can be a list for selection.
-
-            Returns: `dict`:
-                - Key input_fields_group (`string`): Can be either required, hidden or optional. A node class must have property `required`
-                - Value input_fields (`dict`): Contains input fields config:
-                    * Key field_name (`string`): Name of a entry-point method's argument
-                    * Value field_config (`tuple`):
-                        + First value is a string indicate the type of field or a list for selection.
-                        + Secound value is a config for type "INT", "STRING" or "FLOAT".
-        """
         return {
             "required": {
                 "images": ("IMAGE",),
                 "query": ("STRING", {
-                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "multiline": True,
                     "default": "describe the image"
                 }),
                 "debug": (["enable", "disable"],),
                 "url": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
+                    "multiline": False,
                     "default": "http://127.0.0.1:11434"
                 }),
                 "model": ("STRING", {
-                    "multiline": False, #True if you want the field to look like the one on the ClipTextEncode node
+                    "multiline": False,
                     "default": "llava"
                 }),
                 
@@ -72,7 +32,7 @@ class OllamaVision:
         }
 
     RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("text",)
+    RETURN_NAMES = ("description",)
 
     FUNCTION = "vision"
 
@@ -81,14 +41,15 @@ class OllamaVision:
     CATEGORY = "Ollama"
 
     def vision(self, images, query, debug, url, model):
-        img_str = ""
+        images_b64 = []
         for (batch_number, image) in enumerate(images):
             i = 255. * image.cpu().numpy()
             img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
     
             buffered = BytesIO()
             img.save(buffered, format="PNG")
-            img_str = base64.b64encode(buffered.getvalue())
+            img_bytes = base64.b64encode(buffered.getvalue())
+            images_b64.append(str(img_bytes, 'utf-8'))
         if debug == "enable":
             print(f"""Your input contains:
                 query: {query}
@@ -101,10 +62,9 @@ class OllamaVision:
           {
             'role': 'user',
             'content': query,
-            'image': str(img_str, 'utf-8'),
+            'images': images_b64,
           },
         ])
-        print(response['message']['content'])
         return (response['message']['content'],)
 
 NODE_CLASS_MAPPINGS = {
@@ -112,5 +72,5 @@ NODE_CLASS_MAPPINGS = {
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "OllamaVision": "Perform vision query",
+    "OllamaVision": "Ollama Vision",
 }
