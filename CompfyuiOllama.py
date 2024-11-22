5eb23318fe1ccf1dd408827701755a66b05b8850
Stav Sapir <stavsap@gmail.com>
2024-11-22 15:56:09 +0200

    v2 nodes init

--- CompfyuiOllama.py
+++ CompfyuiOllama.py
@@ -14,14 +14,16 @@ from PIL import Image
 from PIL.PngImagePlugin import PngInfo
 import os
 
+
 @PromptServer.instance.routes.post("/ollama/get_models")
 async def get_models_endpoint(request):
     data = await request.json()
     url = data.get("url")
     client = Client(host=url)
     models = [model['name'] for model in client.list().get('models', [])]
-    
+
     return web.json_response(models)
+
 
 class OllamaVision:
     def __init__(self):
@@ -44,7 +46,7 @@ class OllamaVision:
                 }),
                 "model": ((), {}),
                 "keep_alive": ("INT", {"default": 5, "min": -1, "max": 60, "step": 1}),
-                "format": (["text", "json",''],),
+                "format": (["text", "json", ''],),
                 "seed": ("INT", {"default": seed, "min": 0, "max": 2 ** 31, "step": 1}),
             },
         }
@@ -80,7 +82,8 @@ request query params:
 
 """)
 
-        response = client.generate(model=model, prompt=query, images=images_b64, keep_alive=str(keep_alive) + "m", format=format)
+        response = client.generate(model=model, prompt=query, images=images_b64, keep_alive=str(keep_alive) + "m",
+                                   format=format)
 
         if debug == "enable":
             print("[Ollama Vision]\nResponse:\n")
@@ -108,7 +111,7 @@ class OllamaGenerate:
                 }),
                 "model": ((), {}),
                 "keep_alive": ("INT", {"default": 5, "min": -1, "max": 60, "step": 1}),
-                "format": (["text", "json",''],),
+                "format": (["text", "json", ''],),
             },
         }
 
@@ -137,10 +140,11 @@ request query params:
         response = client.generate(model=model, prompt=prompt, keep_alive=str(keep_alive) + "m", format=format)
 
         if debug == "enable":
-                print("[Ollama Generate]\nResponse:\n")
-                pprint(response)
+            print("[Ollama Generate]\nResponse:\n")
+            pprint(response)
 
         return (response['response'],)
+
 
 # https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
 
@@ -168,7 +172,7 @@ class OllamaGenerateAdvance:
                 "system": ("STRING", {
                     "multiline": True,
                     "default": "You are an art expert, gracefully describing your knowledge in art domain.",
-                    "title":"system"
+                    "title": "system"
                 }),
                 "seed": ("INT", {"default": seed, "min": 0, "max": 2 ** 31, "step": 1}),
                 "top_k": ("INT", {"default": 40, "min": 0, "max": 100, "step": 1}),
@@ -178,8 +182,8 @@ class OllamaGenerateAdvance:
                 "tfs_z": ("FLOAT", {"default": 1, "min": 1, "max": 1000, "step": 0.05}),
                 "keep_alive": ("INT", {"default": 5, "min": -1, "max": 60, "step": 1}),
                 "keep_context": ("BOOLEAN", {"default": False}),
-                "format": (["text", "json",''],),
-            },"optional": {
+                "format": (["text", "json", ''],),
+            }, "optional": {
                 "context": ("STRING", {"forceInput": True}),
             }
         }
@@ -189,7 +193,8 @@ class OllamaGenerateAdvance:
     FUNCTION = "ollama_generate_advance"
     CATEGORY = "Ollama"
 
-    def ollama_generate_advance(self, prompt, debug, url, model, system, seed, top_k, top_p,temperature, num_predict, tfs_z, keep_alive, keep_context, format, context=None):
+    def ollama_generate_advance(self, prompt, debug, url, model, system, seed, top_k, top_p, temperature, num_predict,
+                                tfs_z, keep_alive, keep_context, format, context=None):
 
         client = Client(host=url)
 
@@ -216,11 +221,11 @@ class OllamaGenerateAdvance:
 
         options = {
             "seed": seed,
-            "top_k":top_k,
-            "top_p":top_p,
-            "temperature":temperature,
-            "num_predict":num_predict,
-            "tfs_z":tfs_z,
+            "top_k": top_k,
+            "top_p": top_p,
+            "temperature": temperature,
+            "num_predict": num_predict,
+            "tfs_z": tfs_z,
         }
 
         if context != None and isinstance(context, str):
@@ -240,7 +245,8 @@ request query params:
 - options: {options}
 """)
 
-        response = client.generate(model=model, system=system, prompt=prompt, context=context, options=options, keep_alive=str(keep_alive) + "m", format=format)
+        response = client.generate(model=model, system=system, prompt=prompt, context=context, options=options,
+                                   keep_alive=str(keep_alive) + "m", format=format)
         if debug:
             print("[Ollama Generate Advance]\nResponse:\n")
             pprint(response)
@@ -250,13 +256,15 @@ request query params:
 
         return (response['response'], response['context'],)
 
+
 class OllamaSaveContext:
     def __init__(self):
         self._base_dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "saved_context"
+
     @classmethod
     def INPUT_TYPES(s):
         return {"required":
-                    {"context": ("STRING", {"forceInput": True}, ),
+                    {"context": ("STRING", {"forceInput": True},),
                      "filename": ("STRING", {"default": "context"})},
                 }
 
@@ -274,7 +282,7 @@ class OllamaSaveContext:
 
         image = Image.new('RGB', (100, 100), (255, 255, 255))  # Creates a 100x100 white image
 
-        image.save(path+".png", pnginfo=metadata)
+        image.save(path + ".png", pnginfo=metadata)
 
         return {"ui": {"context": context}}
 
@@ -282,6 +290,7 @@ class OllamaSaveContext:
 class OllamaLoadContext:
     def __init__(self):
         self._base_dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "saved_context"
+
     @classmethod
     def INPUT_TYPES(s):
         input_dir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "saved_context"
@@ -312,8 +321,33 @@ class OllamaOptions:
         seed = random.randint(1, 2 ** 31)
         return {
             "required": {
-                "format": (["text", "json",''],),
+                "enable_mirostat": ("BOOLEAN", {"default": False}),
+                "mirostat": ("INT", {"default": 0, "min": 0, "max":2, "step": 1}),
+                "enable_mirostat_eta": ("BOOLEAN", {"default": False}),
+                "mirostat_eta": ("FLOAT", {"default": 0.1, "min": 0, "step": 0.1}),
+                "enable_mirostat_tau": ("BOOLEAN", {"default": False}),
+                "mirostat_tau": ("FLOAT", {"default": 5.0, "min": 0, "step": 0.1}),
+                "enable_num_ctx": ("BOOLEAN", {"default": False}),
+                "num_ctx": ("INT", {"default": 2048, "min": 0, "step": 1}),
+                "enable_repeat_last_n": ("BOOLEAN", {"default": False}),
+                "repeat_last_n": ("INT", {"default": 64, "min": -1, "max": 64, "step": 1}),
+                "enable_repeat_penalty": ("BOOLEAN", {"default": False}),
+                "repeat_penalty": ("FLOAT", {"default": 1.1, "min": 0, "max": 2, "step": 0.05}),
+                "enable_temperature": ("BOOLEAN", {"default": False}),
+                "temperature": ("FLOAT", {"default": 0.8, "min": 0, "max": 1, "step": 0.05}),
+                "enable_seed": ("BOOLEAN", {"default": False}),
                 "seed": ("INT", {"default": seed, "min": 0, "max": 2 ** 31, "step": 1}),
+                "enable_tfs_z": ("BOOLEAN", {"default": False}),
+                "tfs_z": ("FLOAT", {"default": 1, "min": 1, "max": 1000, "step": 0.05}),
+                "enable_num_predict": ("BOOLEAN", {"default": False}),
+                "num_predict": ("INT", {"default": -1, "min": -2, "max": 2048, "step": 1}),
+                "enable_top_k": ("BOOLEAN", {"default": False}),
+                "top_k": ("INT", {"default": 40, "min": 0, "max": 100, "step": 1}),
+                "enable_top_p": ("BOOLEAN", {"default": False}),
+                "top_p": ("FLOAT", {"default": 0.9, "min": 0, "max": 1, "step": 0.05}),
+                "enable_min_p": ("BOOLEAN", {"default": False}),
+                "min_p": ("FLOAT", {"default": 0.0, "min": 0, "max": 1, "step": 0.05}),
+                "debug": ("BOOLEAN", {"default": False}),
             },
         }
 
@@ -322,12 +356,9 @@ class OllamaOptions:
     FUNCTION = "ollama_options"
     CATEGORY = "Ollama"
 
-    def ollama_options(self, format, seed):
-        data = {
-            "format": format,
-            "seed": seed,
-        }
-        return (data,)
+    def ollama_options(self, **kargs):
+        pprint(kargs)
+        return (kargs,)
 
 class OllamaConnectivity:
     def __init__(self):
@@ -344,7 +375,6 @@ class OllamaConnectivity:
                 "model": ((), {}),
                 "keep_alive": ("INT", {"default": 5, "min": -1, "max": 120, "step": 1}),
                 "keep_alive_unit": (["minutes", "hours"],),
-                "debug": ("BOOLEAN", {"default": False}),
             },
         }
 
@@ -353,61 +383,85 @@ class OllamaConnectivity:
     FUNCTION = "ollama_connectivity"
     CATEGORY = "Ollama"
 
-    def ollama_connectivity(self, url, model, keep_alive, keep_alive_unit, debug):
-
+    def ollama_connectivity(self, url, model, keep_alive, keep_alive_unit):
         data = {
             "url": url,
             "model": model,
             "keep_alive": keep_alive,
             "keep_alive_unit": keep_alive_unit,
-            "debug": debug
         }
-
-        if debug:
-            pprint(data)
 
         return (data,)
 
+
 class OllamaGenerateV2:
     def __init__(self):
-        pass
+        self.saved_context = None
 
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-
+                "system": ("STRING", {
+                    "multiline": True,
+                    "default": "you are an AI artist"
+                }),
                 "prompt": ("STRING", {
                     "multiline": True,
                     "default": "what is art?"
                 }),
+                "keep_context": ("BOOLEAN", {"default": False}),
+                "format": (["text", "json", ''],),
+
             },
-            "optional":{
+            "optional": {
                 "options": ("OLLAMA_OPTIONS", {"forceInput": False},),
                 "connectivity": ("OLLAMA_CONNECTIVITY", {"forceInput": False},),
                 "images": ("IMAGE", {"forceInput": False},),
+                "context": ("OLLAMA_CONTEXT", {"forceInput": False},),
                 "meta": ("OLLAMA_META", {"forceInput": False},),
             }
         }
+
     RETURN_TYPES = ("STRING", "OLLAMA_CONTEXT", "OLLAMA_META",)
     RETURN_NAMES = ("result", "context", "meta",)
     FUNCTION = "ollama_generate_v2"
     CATEGORY = "Ollama"
 
-    def ollama_generate_v2(self, prompt, options= None, connectivity= None, images = None, meta = None):
+    def ollama_generate_v2(self, system, prompt, format, keep_context, context = None, options=None, connectivity=None, images=None, meta=None):
 
         if connectivity is None and meta is None:
-            raise Exception("You must specify connectivity or meta")
+            raise Exception("You must input connectivity or meta")
 
         if meta is not None:
-            if connectivity is not None:
+            if connectivity is not None: # bypass the current connectivity
                 meta["connectivity"] = connectivity
-            if options is not None:
+            if options is not None: # set/bypass the current options
                 meta["options"] = options
         else:
-            meta = {"options":options, "connectivity":connectivity}
+            meta = {"options": options, "connectivity": connectivity}
 
-        return prompt, [1, 2, 3, 4], meta,
+        client = Client(host=meta['connectivity']['url'])
+
+        if format == "text":
+            format = ''
+
+        if context is not None and isinstance(context, str):
+            string_list = context.split(',')
+            context = [int(item.strip()) for item in string_list]
+
+        if keep_context and context is None:
+            context = self.saved_context
+
+        keep_alive_unit =  'm' if meta['connectivity']['keep_alive_unit'] == "minutes" else 'h'
+        response = client.generate(model=meta['connectivity']['model'], system=system, prompt=prompt, context=context, options=options,
+                                   keep_alive=str(meta['connectivity']['keep_alive']) + keep_alive_unit, format=format)
+
+        if keep_context:
+            self.saved_context = response["context"]
+
+        return response['response'], response['context'], meta,
+
 
 NODE_CLASS_MAPPINGS = {
     "OllamaVision": OllamaVision,
